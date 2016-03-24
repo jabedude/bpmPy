19e732c7cac43e3da1d8db5df3b586ed051c6653
Joshua Abraham <joshuaabraham@Joshuas-MacBook-Air.local>
2016-03-24 18:30:05 -0400

    Added where to put api tokens

--- bpmGUIOpen.py
+++ bpmGUIOpen.py
@@ -10,8 +10,8 @@ top = Tkinter.Tk()
 var = StringVar()
 label = Label(top, textvariable = var, relief = RAISED)
 def playSong():
-    auth = tweepy.OAuthHandler('xxx','xxx')
-    auth.set_access_token('xxx','xxx')
+    auth = tweepy.OAuthHandler('consumer key here','consumer secret here')
+    auth.set_access_token('access token here','access token secret here')
     api = tweepy.API(auth)
     bpmTwit = api.user_timeline('bpm_playlist')
     for tweet in bpmTwit:
